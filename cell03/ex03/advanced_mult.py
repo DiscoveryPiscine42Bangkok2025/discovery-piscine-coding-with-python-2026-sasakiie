49d724b7836cdf3d110fa6364b5fbae8054a26e2
Phawothai Na pairee <bright4869napairee@gmail.com>
2026-02-11 18:01:29 +0700

    submit exercise

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,9 +1,15 @@
-x = 0
-while x <= 10: 
-    print(f"Table de {x}:", end=" ")
-    j = 0
-    while j <= 10: 
-        print(x * j, end=" ")
-        j += 1
-    print() 
-    x += 1
+import sys
+
+# โจทย์ระบุว่าถ้ามี argument (len > 1) ให้ปริ้น none
+if len(sys.argv) > 1:
+    print("none")
+else:
+    x = 0
+    while x <= 10:
+        print(f"Table de {x}:", end=" ")
+        j = 0
+        while j <= 10:
+            print(x * j, end=" ")
+            j += 1
+        print()
+        x += 1
